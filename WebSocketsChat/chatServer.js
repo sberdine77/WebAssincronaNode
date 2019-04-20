cafd68cda7fe966474e5d52be74973891858426c
raquel <sbsx77xavier@hotmail.com>
2019-04-20 18:12:45 -0300

    Adding chat server logic

---
+++ WebSocketsChat/chatServer.js
@@ -0,0 +1,80 @@
+process.title = "node-chat";
+
+var webSocketsServerPort = 1337;
+
+var websocketServer = requeire('websocket').server;
+var http = require('http');
+
+var history = [];
+var users = []
+
+function htmlEntities(str) {
+    return String(str)
+      .replace(/&/g, '&amp;').replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+var colors = ['red', 'blue', 'green', 'magenta', 'purple', 'plum', 'orange'];
+colors.sort(function(a,b) { return Math.random() > 0.5; } );
+
+var server = http.createServer(function(request, response) {
+    console.log("Server is listening on port " + webSocketsServerPort);
+});
+
+var wsServer = new websocketServer({
+    httpServer = server
+});
+
+wsServer.on('request', function(request) {
+    console.log((new Date()) + 'Connection request from ' + request.origin);
+    var connection = request.accept(null, request.origin);
+    var index = users.push(connection) - 1;
+    var userName = false;
+    var userColor = false;
+    
+    console.log((new Date()) + 'Connection accepted.');
+    if (history.length > 0) {
+        connection.sendUTF(JSON.stringify({
+            type: 'history', data: history
+        }));
+    }
+
+    connection.on('message', function(message) {
+        if (message.type === 'utf-8') {
+            if (userName === false) {
+                userName = htmlEntities(message.utf8Data);
+                userColor = colors.shift();
+                connection.sendUTF(JSON.stringify({
+                    type: 'color', data: userColor
+                }));
+                console.log((new Date()) + ' User is known as: ' + userName + ' with ' + userColor + ' color.')
+            } else {
+                console.log((new Date()) + ' Received Message from ' + userName + ': ' + message.utf8Data);
+                var obj = {
+                  time: (new Date()).getTime(),
+                  text: htmlEntities(message.utf8Data),
+                  author: userName,
+                  color: userColor
+                };
+                history.push(obj);
+                history = history.slice(-100);
+                var json = JSON.stringify({
+                    type:'message', data: obj
+                });
+                for (var i = 0; i < users.length; i++) {
+                  users[i].sendUTF(json);
+                }
+            }
+        }
+    });
+
+    connection.on('close', function(connection) {
+        if (userName !== false && userColor !== false) {
+            console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
+            users.splice(index, 1);
+            colors.push(userColor);
+        }
+    });
+
+});
+
